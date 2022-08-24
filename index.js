08f156ed38b17269e2b46f18259be485faa74243
Roshan P R <99485730+roshan-pr@users.noreply.github.com>
2022-08-24 08:15:30 +0530

    Customized players joining flow

--- index.js
+++ index.js
@@ -1,23 +1,47 @@
 const { visitPage, login, hostGame, getRoomId, joinGame } = require('./lib.js');
 
-const count = +process.argv[2];
-const PLAYERS_COUNT = count <= 6 && count >= 3 ? count : 3;
-const IP = process.argv[3] || '127.0.0.1';
-const PORT = process.argv[4] || 8000;
+const loginUser = (url, username) => visitPage(url).then(login(username));
 
-const url = `http://${IP}:${PORT}`;
-const players = ['Barnali', 'Prajakta', 'Chhavi', 'Prem', 'Sakshi', 'Azhar']
-
-const loginUser = (user) => visitPage(url).then(login(user));
-
-const startGame = ([hostPage, ...joineePages]) => {
-  hostGame(hostPage, `${PLAYERS_COUNT}`)
+const startGame = ([hostPage, ...joineePages], numOfPlayers) => {
+  hostGame(hostPage, numOfPlayers)
     .then(getRoomId)
     .then(roomId => joineePages.map(joinGame(roomId)))
 };
 
-const main = () =>
-  Promise.all(players.slice(0, PLAYERS_COUNT).map(loginUser))
-    .then(startGame);
+const joinToGame = (joineePages, roomId) =>
+  joineePages.map(joinGame(roomId));
 
-main();
+const main = ([option, numOfPlayers, ...inputs]) => {
+  let ip;
+  let roomId;
+  if (numOfPlayers < 2 || numOfPlayers > 6) {
+    console.log('Number of players should be in range 3 to 6');
+    return;
+  }
+
+  if (option === '-j') {
+    console.log('Make sure the game is hosted!!!');
+    roomId = inputs[0];
+    ip = inputs[1] || '127.0.0.1'; // IP address
+  }
+  else if (option === '-l') {
+    ip = inputs[0] || '127.0.0.1'; // IP address
+  }
+  else {
+    console.log('illigal option');
+    return;
+  }
+  const url = `http://${ip}:8000`;
+  const players = ['Barnali', 'Prajakta', 'Chhavi', 'Prem', 'Sakshi', 'Azhar'].slice(0, numOfPlayers);
+
+  Promise.all(players.map(player =>
+    loginUser(url, player)))
+    .then(pages => {
+      if (option === '-l')
+        startGame(pages, numOfPlayers + '');
+      if (option === '-j')
+        joinToGame(pages, roomId + '');
+    });
+};
+
+main(process.argv.slice(2));
